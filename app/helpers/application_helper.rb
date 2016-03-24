f121c3270c9d085a35d8d09ea9806e3560caf536
alexbarron <alexbarron@gmail.com>
2016-03-24 11:50:35 -0700

    Permitted link helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def permitted_links(model)
+    if permitted?(model)
+      simple_format(link_to("Edit #{model.name}", edit_polymorphic_path(model), class: "btn btn-primary")) + 
+      simple_format(link_to("Delete #{model.name}", polymorphic_path(model), method: :delete, data: {confirm: "Are you sure?"}, class: "btn btn-danger"))
+    end
+  end
+
+  def permitted?(model)
+    if model.class.name == "Player"
+      admin_check?
+    else
+      admin_check? || current_user == model.user
+    end
+  end
+
+  def admin_check?
+    !!current_user && current_user.admin?
+  end
+
 end
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,22 @@
 module PlayersHelper
+
+  def add_player_column(player)
+    if current_user.team.players.include?(player)
+      "This player is on your team"
+    else
+      link_to "Add Player", roster_spots_path(player_id: player.id), method: :post, class: "btn btn-success btn-xs"
+    end
+  end
+
+  def has_team_check
+    if !!current_user && !!current_user.team
+      render 'players/has_team_options'
+    end
+  end
+
+  def admin_options
+    if admin_check?
+      render 'players/admin_player_options'
+    end
+  end
 end
